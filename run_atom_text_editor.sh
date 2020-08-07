9622c9695485e5ed4b66bccb2256554901141018
Andrew Mbugua <51198360+AndrewMbugua@users.noreply.github.com>
2020-08-07 18:00:49 +0300

    Update run_atom_text_editor.sh

--- run_atom_text_editor.sh
+++ run_atom_text_editor.sh
@@ -2,4 +2,9 @@
 
 # running atom woth the no sandbox flag via terminal
 
+# After updating Atom text editor it seems I require to run --no-sandbox flag but after a while it becomes boring so I wrote a simple BASH script to be doing this for me:
+
 eval "atom --no-sandbox flag"
+
+# just save this in a common directory that you frequently use and type ./atom_text_editor.sh in the terminal to deploy(Depending on the name you choose for your script)
+
