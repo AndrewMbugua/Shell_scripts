d27dcdbcf98175ddfd6d72abd1a8cfce1f3370d8
Andrew Mbugua <51198360+AndrewMbugua@users.noreply.github.com>
2020-10-02 17:29:05 +0000

    Create batteryLevel.sh

---
+++ batteryLevel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#THIS IS AN INCOMPLETE SCRIPT!
+
+#This tries to display battery level and tell you the exact level its in
+
+lowBattery=percentage 20%
+upower -i `upower -e | grep 'BAT'` >> battery.txt
+
+#display battery % only
+grep -i "percentage" battery.txt >> percentage.txt
+
+#Display the last lines of percentage file
+cat percentage.txt | tail -1
+
+#kinda wrong
+if cat percentage.txt | tail -1 -eq $lowBattery
+then
+echo "yees"
+
+fi
+
